00ff1ff2182d16c04a814c1ec899c76f6d9ffc3c
Pedro Vitor <pedro.vitore19@gmail.com>
2023-03-01 23:51:56 -0300

    Calculator

---
+++ first-project-with-spring-boot/src/main/java/br/com/pedro/controller/CalculatorController.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.pedro.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Pedro Vitor
+ */
+@RestController
+public class CalculatorController {
+    
+    // GET
+    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double sum(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) {
+        return 1D;
+    }
+}
